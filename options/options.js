40879d8cfaa8edb73083ac0b6d8aa8595e093b42
Crowdbotics <team@crowdbotics.com>
2024-02-16 16:37:46 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hotel-reviews-46403",
+  url: "https://hotel-reviews-46403.botics.co",
+  api: "https://hotel-reviews-46403.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
